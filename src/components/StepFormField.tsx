403f59ad9ca3746e5af51d93aebb4cec1b50e7d5
patson120 <patrickkennenl@gmail.com>
2026-03-17 08:04:19 -0300

    Update

--- src/components/StepFormField.tsx
+++ src/components/StepFormField.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { View, StyleSheet } from 'react-native';
 import { Checkbox, HelperText, Text, TextInput } from 'react-native-paper';
 import { Controller } from 'react-hook-form';
